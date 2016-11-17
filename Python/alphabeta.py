1b1b1f30c0c6eb80ded2139682400a18c9b11d91
RonHedge904 <rlahaye904@gmail.com>
2016-11-17 15:38:16 +0100

    alphabeta deuxième commit (classe alphabeta)

--- Python/alphabeta.py
+++ Python/alphabeta.py
@@ -3,7 +3,7 @@ import hex_game
 from tools import *
 
 
-class PlayerMiniMax:
+class PlayerAlphaBeta:
     def init(self, renderer):
         pass
 
@@ -18,33 +18,11 @@ class PlayerMiniMax:
         depth = 9000
         moves = [k for k in numpy.argwhere(board == -1) if 0 != k[0] != board.shape[0] != k[1] != 0]
         move = moves[numpy.argmax(
-            [-self.minimax(hex_game.play_move_copy(board, move[0], move[1], player), depth, 1 - player) for move in
+            [-self.alphabeta(hex_game.play_move_copy(board, move[0], move[1], player), depth, 1 - player, -float.infinity, float.infinity) for move in
              moves])]
         return hex_game.play_move(board, move[0], move[1], player)
 
-    def minimax(self, board: numpy.ndarray, depth: int, player: int) -> float:
-        """
 
-        :param board: numpy array
-        :param depth: int
-        :param play_move: param : board, player, move -> board
-        :param get_score: param : board, -> score player0 - score player1
-        :param get_moves: param : board, player -> tuple list
-        :return: score player0-score player1
-        """
-        self.count += 1
-        if self.count % 100 == 0:
-            print(self.count)
-        if depth == 0:
-            return get_scores(board)[0][player]
-        else:
-            moves = [k for k in numpy.argwhere(board == -1) if 0 != k[0] != board.shape[0] != k[1] != 0]
-            if len(moves) == 0:
-                return get_scores(board)[0][player]
-            else:
-                return max(
-                    [-self.minimax(hex_game.play_move_copy(board, move[0], move[1], player), depth - 1, 1 - player)
-                     for move in moves])
 
     def alphabeta(self, board: numpy.ndarray, depth: int, player: int, alpha: float, beta: float):
 
