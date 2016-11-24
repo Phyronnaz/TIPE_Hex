004b986e32914a01522b66d9ab22061ada0df55b
RonHedge904 <rlahaye904@gmail.com>
2016-11-24 15:52:19 +0100

    alphabeta marche !!!!!!

--- Python/launcher.py
+++ Python/launcher.py
@@ -2,6 +2,6 @@ from minmax import PlayerMiniMax
 from game_handler import GameHandler
 from player_human import HumanPlayer
 from ai_poisson import PoissonAI
-from player import Player
+from alphabeta import PlayerAlphaBeta
 
-GameHandler(HumanPlayer(), HumanPlayer(), visual_size=11)
+GameHandler(PlayerAlphaBeta(), HumanPlayer(), visual_size=3)
