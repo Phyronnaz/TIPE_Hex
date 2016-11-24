a9875f0cf8b08f5fdd45a7ca62ee0f3a006a2edd
Phyronnaz <phyronnaz@gmail.com>
2016-11-24 22:19:27 +0100

    Update launcher

--- Python/launcher.py
+++ Python/launcher.py
@@ -3,5 +3,6 @@ from game_handler import GameHandler
 from player_human import HumanPlayer
 from ai_poisson import PoissonAI
 from player import Player
+from alphabeta import PlayerAlphaBeta
 
 GameHandler(HumanPlayer(), HumanPlayer(), visual_size=11)
