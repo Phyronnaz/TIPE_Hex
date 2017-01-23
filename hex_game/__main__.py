5b6a7a2b1df0e879a42fb07d08db1a51f70e2a79
Phyronnaz <phyronnaz@gmail.com>
2017-01-23 22:19:09 +0100

    notebook

--- hex_game/__main__.py
+++ hex_game/__main__.py
@@ -1,13 +1,16 @@
 from .q_learning import learn
 from hex_game.game import Game
 from hex_game.players import *
+import keras.models
 
-for gamma in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-    learn(size=5, epochs=250000, gamma=gamma, first_player=0, early_reward=False, save_path="/notebooks/admin/saves",
-          save=True)
+# for gamma in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+#     learn(size=5, epochs=250000, gamma=gamma, first_player=False, early_reward=False, save_path="/notebooks/admin/saves",
+#           save=True)
 
-# a = RandomPlayer()
-# b = QPlayer(model)
-# player0, player1 = (a, b) if player == 1 else (b, a)
-# game = Game(size=size, player0=player0, player1=player1)
-# game.start()
+model = keras.models.load_model("/notebooks/admin/model.model")
+
+a = HumanPlayer()
+b = QPlayer(model)
+game = Game(size=5, player0=a, player1=b)
+a.init(game)
+game.start()
