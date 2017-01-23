04c635bd917b9a2cef6410d269c9f3ac9a8d91b5
Phyronnaz <phyronnaz@gmail.com>
2017-01-23 01:39:56 +0100

    pandas

--- hex_game/__main__.py
+++ hex_game/__main__.py
@@ -1,44 +1,10 @@
 from .q_learning import learn
 from hex_game.game import Game
 from hex_game.players import *
-from bokeh.plotting import output_file, show, figure, gridplot
-from bokeh.palettes import Category10
-import numpy
 
-#TODO: numpy.savez
-
-epochs = 250000
-
-path = "/home/admin/saves/2017-01-20/"
-
-for size in [5]:
-    figs = [0, 1]
-    for early_reward in [False]:
-        loss_array = []
-        l = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-        for gamma in l:
-            model, loss = learn(size=size, epochs=epochs, gamma=gamma, player=0, early_reward=early_reward)
-            loss_array.append(loss)
-
-            name = "size-{}_gamma-{}_player-{}_early_reward-{}_epochs-{}".format(size, gamma, 0, early_reward, epochs)
-
-            model.save(path + name + ".model")
-            numpy.savetxt(path + name + ".npy", loss)
-
-        name = "size={}, early reward={}".format(size, early_reward)
-        f = figure(title=name, x_axis_label='epochs', y_axis_label='loss', webgl=True,
-                   tools="pan,wheel_zoom,box_zoom,reset,resize,save,crosshair")
-
-        for i in range(len(l)):
-            f.circle(loss_array[i][:, 0], loss_array[i][:, 1], legend="gamma=" + str(l[i]), alpha=0.2,
-                     color=Category10[10][i], line_color=None)
-
-        figs[int(early_reward)] = f
-
-    output_file(path + "output_" + str(size) + ".html")
-
-    f = gridplot([figs])
-    show(f)
+for gamma in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+    learn(size=5, epochs=250000, gamma=gamma, first_player=0, early_reward=False, save_path="/notebooks/admin/saves",
+          save=True)
 
 # a = RandomPlayer()
 # b = QPlayer(model)
--- hex_game/q_learning.py
+++ hex_game/q_learning.py
@@ -1,14 +1,13 @@
-import numpy
 import random
 import time
 import datetime
+import numpy as np
+import pandas as pd
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import RMSprop
 from keras.callbacks import History
 from keras.utils.visualize_util import plot
-from bokeh.plotting import output_file, show, figure, gridplot
-from hex_game.players import *
 from hex_game.main import *
 from hex_game.winner_check import *
 from hex_game.utils import *
@@ -27,119 +26,146 @@ def init_model(size):
     return model
 
 
-def learn(size=3, epochs=25000, gamma=0.8, player=0, early_reward=False):
-    batch_size = 1
-    history = History()
+def learn(size=3, epochs=25000, gamma=0.8, first_player=True, early_reward=False, save_path="/notebooks/admin/saves",
+          save=True):
+    """
+    Train the model
+    :param size: size of the game
+    :param epochs: number of games
+    :param gamma: gamma for Q learning
+    :param first_player: is AI beginning?
+    :param early_reward: if no winner and no mistake, reward = move count
+    :param save_path: path of the save folder
+    :param save: save results?
+    :return: model, dataframe
+    """
 
     model = init_model(size)
     # plot(model, to_file='model.png')
 
-
     epsilon = 1
 
-    move_count_array = numpy.zeros(epochs)
-    winner_array = numpy.zeros(epochs)
-    loss = numpy.zeros((epochs * size ** 2, 2))
-    loss_c = 0
+    index = np.arange(0, epochs * size ** 2)
+    epoch_ser = pd.Series(index=index)
+    winner_ser = pd.Series(index=index)
+    epsilon_ser = pd.Series(index=index)
+    random_move_ser = pd.Series(index=index, dtype='bool')
+    loss_ser = pd.Series(index=index)
+    reward_ser = pd.Series(index=index)
+    move_count_ser = pd.Series(index=index)
+    random_ai_move_ser = pd.Series(index=index, dtype='bool')
+
+    ser_counter = 0
 
     start_time = time.time()
 
-    print("Epochs : {}, Batch size: {}, gamma: {}, size : {}, early reward : {}, player : {}".
-          format(epochs, batch_size, gamma, size, early_reward, player))
+    print("Epochs : {}, gamma: {}, size : {}, early reward : {}, first player : {}".
+          format(epochs, gamma, size, early_reward, first_player))
 
-    for i in range(epochs):
-        for j in range(batch_size):
-            board = init_board(size)
-            winner_matrix, winner_counter = init_winner_matrix_and_counter(size)
-            random_state = numpy.random.RandomState(i)
-            winner = -1
-            move_count = 0
-            while winner == -1:
-                if player == move_count % 2:
-                    # Save board
-                    splitted_board = get_splitted_board(board, player)
-                    # Get move
-                    qval = model.predict(splitted_board, batch_size=batch_size)
-                    random_move = random.random() < epsilon
-                    if random_move:
-                        action = numpy.random.randint(0, size ** 2)
-                    else:
-                        action = numpy.argmax(qval)
-                    move = action // size, action % size
+    for epoch in range(epochs):
+        board = init_board(size)
+        winner_matrix, winner_counter = init_winner_matrix_and_counter(size)
+        random_state = np.random.RandomState(epoch)
+        winner = -1
+        move_count = 0
+        player = 0
+        while winner == -1:
+            if (first_player and player == 0) or (not first_player and player == 1):
+                # Save board
+                split_board = get_splitted_board(board, player)
+                # Get move
+                qval = model.predict(split_board, batch_size=1)
+                random_move = random.random() < epsilon
+                action = np.random.randint(0, size ** 2) if random_move else np.argmax(qval)
+                move = action // size, action % size
 
-                    # Play move
-                    if can_play_move(board, move):
-                        board[move] = player
-                        winner, winner_counter = check_for_winner(move, player, winner_matrix, winner_counter)
-                        move_count += 1
-                    else:
-                        winner = 3
-
-                    # Observe reward
-                    if winner == -1:
-                        reward = move_count if early_reward else 0
-                    elif winner == player:
-                        reward = 2 * size ** 2
-                    elif winner == 1 - player:
-                        reward = -2 * size ** 2
-                    elif winner == 3:
-                        reward = -2 * size ** 2
-
-                    # Get max_Q(S',a)
-                    newQ = model.predict(get_splitted_board(board, player), batch_size=batch_size)
-                    maxQ = numpy.max(newQ)
-
-                    # Get update
-                    if winner == -1:
-                        update = reward + gamma * maxQ
-                    else:
-                        update = reward
-
-                    qval[0][action] = update
-                    model.fit(splitted_board, qval, batch_size=1, nb_epoch=1, verbose=0, callbacks=[history])
-                    # Log
-                    if not random_move:
-                        loss[loss_c, 0] = i + move_count / size ** 2
-                        loss[loss_c, 1] = history.history["loss"][0]
-                        loss_c += 1
-                else:
-                    # AI
-                    move = get_move_random(board, random_state)
-                    board[move] = 1 - player
-                    winner, winner_counter = check_for_winner(move, 1 - player, winner_matrix, winner_counter)
+                # Play move
+                if can_play_move(board, move):
+                    board[move] = player
+                    winner, winner_counter = check_for_winner(move, player, winner_matrix, winner_counter)
                     move_count += 1
-            move_count_array[i] = move_count
-            winner_array[i] = winner
+                else:
+                    winner = 2
 
-            # Log
-            if i % 1000 == 0 and i != 0:
-                w_a = winner_array[max(i - 1000, 0): i]
-                m_a = move_count_array[max(i - 1000, 0): i]
-                l_a = loss[max(loss_c - 1000, 0): loss_c, 1]
-                w = round(w_a.mean(), 2)
-                m = round(m_a.mean(), 2)
-                l = round(l_a.mean(), 2)
+                # Observe reward
+                if winner == -1:
+                    reward = move_count if early_reward else 0
+                elif winner == player:
+                    reward = 2 * size ** 2
+                elif winner == 1 - player:
+                    reward = -2 * size ** 2
+                elif winner == 2:
+                    reward = -2 * size ** 2
 
-                deltatime = time.time() - start_time
+                # Get max_Q(S',a)
+                newQ = model.predict(get_splitted_board(board, player), batch_size=1)
+                maxQ = np.max(newQ)
 
-                t = round((epochs - i) * deltatime / i, 0)
-                t_s = datetime.datetime.fromtimestamp(t).strftime('%M:%S')
+                # Get update
+                if winner == -1:
+                    update = reward + gamma * maxQ
+                else:
+                    update = reward
 
-                print("Game #: %s | Average Winner %s | Average Move count %s | Average loss %s | Remaining time %s" % (
-                    i, w, m, l, t_s))
+                # Fit the model
+                qval[0][action] = update
+                history = History()
+                model.fit(split_board, qval, batch_size=1, nb_epoch=1, verbose=0, callbacks=[history])
+
+                # Log
+                epoch_ser[ser_counter] = epoch
+                winner_ser[ser_counter] = winner
+                epsilon_ser[ser_counter] = epsilon
+                random_move_ser[ser_counter] = random_move
+                loss_ser[ser_counter] = history.history["loss"][0]
+                reward_ser[ser_counter] = reward
+                move_count_ser[ser_counter] = move_count
+                random_ai_move_ser[ser_counter] = False
+                ser_counter += 1
+            else:
+                # AI
+                move = get_move_random(board, random_state)
+                board[move] = player
+                winner, winner_counter = check_for_winner(move, player, winner_matrix, winner_counter)
+                move_count += 1
+                # Log
+                epoch_ser[ser_counter] = epoch
+                winner_ser[ser_counter] = winner
+                move_count_ser[ser_counter] = move_count
+                random_ai_move_ser[ser_counter] = True
+                ser_counter += 1
+            player = (1 - player) % 2
+        # Log
+        if epoch % 1000 == 0 and epoch != 0:
+            t = round((epochs - epoch) * (time.time() - start_time) / epoch, 0)
+            t_s = datetime.datetime.fromtimestamp(t).strftime('%M:%S')
+            print("Game #: %s | Remaining time %s" % (epoch, t_s))
 
         epsilon -= 1 / epochs
 
-    # Graphs
-    # x = [k for k in range(epochs)]
-    #
-    # f = figure(title="loss")
-    # f.circle(loss[:loss_c, 0], loss[:loss_c, 1], alpha=0.1, line_color=None)
+    columns = (
+        'size', 'gamma', 'first_player', 'early_reward', 'epoch', 'winner', 'epsilon', 'random_move', 'loss', 'reward',
+        'move count')
 
-    # p2 = figure(title="winner")
-    # p2.circle(x, winner_array, alpha=0.1, line_color=None)
-    #
-    # p3 = figure(title="move count")
-    # p3.circle(x, move_count_array, alpha=0.1, line_color=None)
+    df = pd.DataFrame(dict(size=size,
+                           gamma=gamma,
+                           first_player=first_player,
+                           early_reward=early_reward,
+                           epoch=epoch_ser[:ser_counter],
+                           winner=winner_ser[:ser_counter],
+                           epsilon=epsilon_ser[:ser_counter],
+                           random_move=random_move_ser[:ser_counter],
+                           loss=loss_ser[:ser_counter],
+                           reward=reward_ser[:ser_counter],
+                           move_count=move_count_ser[:ser_counter],
+                           random_ai_move=random_ai_move_ser[:ser_counter]
+                           ))
 
-    return model, loss[:loss_c]
+    # Save
+    if save:
+        content = size, gamma, first_player, early_reward, epochs, datetime.datetime.now().isoformat()
+        name = "size-{}-gamma-{}-first_player-{}-early_reward-{}-epochs-{}-date-{}".format(*content)
+        model.save(save_path + "/models/" + name + ".model")
+        df.to_hdf(save_path + "/stats/" + name + ".h5", 'df')
+
+    return model, df
