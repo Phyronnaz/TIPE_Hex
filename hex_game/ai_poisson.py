63ef1b31358e5ab498fb94aa795bd9c4e475162a
Abriel14 <mathieu.vallee1998@gmail.com>
2017-03-26 14:54:59 +0200

    Neighbour matrix construction finished

--- hex_game/ai_poisson.py
+++ hex_game/ai_poisson.py
@@ -3,6 +3,7 @@ import numpy
 from hex_game.main import get_random_move
 from hex_game.poisson import Poisson
 
+NEIGHBORS_1 = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
 
 def get_poisson(board):
     poisson = Poisson(board)
@@ -12,16 +13,29 @@ def get_poisson(board):
     print(U)
     return get_random_move(board, numpy.random.RandomState())
 
-def get_neighbour_matrix():
+def get_neighbour_matrix(poisson_matrix):
+    n = len(poisson_matrix)
+    poisson_matrix = -poisson_matrix
+    neighbour_matrix = 10000 * numpy.ones((n ** 2, n ** 2))
+    for i in range(n):
+        for j in range(i + 1, n):
+            if poisson_matrix[i , j] != 1:
+                neighbour_matrix[11 * i + (j + 1)       , 11 * i + j] = poisson_matrix[i , j]
+                neighbour_matrix[11 * (i + 1) + j       , 11 * i + j] = poisson_matrix[i , j]
+                neighbour_matrix[11 * (i + 1) + (j - 1) , 11 * i + j] = poisson_matrix[i , j]
+                neighbour_matrix[11 * (i - 1) + (j + 1) , 11 * i + j] = poisson_matrix[i , j]
+    return(neighbour_matrix)
 
-    return(neighbour)
-
-def floyd_warshall(W):
+def get_next_floyd_warshall(W):
     n = len(W)
-    for k in range (n):
-        for i in range(n):
-            for j in range (n):
-                W[i,j] = min(W[i,j],W[i,k]+W[k,j])
+    for i in range(n):
+        for j in range (i,n):
+            W[i, j] = min(W[i, j], W[i, k] + W[k, j])
+    return(W)
+
+def get_best_path(W):
+    return()
+
 
 
 
