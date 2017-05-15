7bf7959cad7cf4577bcba1e6db54d64962266d70
Phyronnaz <phyronnaz@gmail.com>
2017-05-15 15:39:30 +0200

    Test single model

--- hex_game/q_learning.py
+++ hex_game/q_learning.py
@@ -270,6 +270,8 @@ def learn(size, gamma, batch_size, initial_epsilon, final_epsilon, exploration_e
             else:
                 models[player] = keras.models.load_model(models_path[player])
 
+    model = models[1]
+
     #####################
     ### Create arrays ###
     #####################
@@ -285,7 +287,7 @@ def learn(size, gamma, batch_size, initial_epsilon, final_epsilon, exploration_e
     #######################
     ### Create memories ###
     #######################
-    memories = [deque(), deque()]
+    memory = deque()
 
     ###########################
     ### Initialize counters ###
@@ -334,39 +336,36 @@ def learn(size, gamma, batch_size, initial_epsilon, final_epsilon, exploration_e
         #################################
         ### Initialize temp variables ###
         #################################
-        actions = [None, None]
-        states = [None, None]
+        # actions = [None, None]
+        # states = [None, None]
         losses = [np.nan, np.nan]
 
         ########################
         ### Learn from memory ##
         ########################
-        for player in q_players:
-            model = models[player]
-            memory = memories[player]
-            if len(memory) >= batch_size:  # enough experiences
-                X = np.zeros((batch_size, size + 4, size + 4, 6))
-                Y = np.zeros((batch_size, size ** 2))
+        if len(memory) >= batch_size:  # enough experiences
+            X = np.zeros((batch_size, size + 4, size + 4, 6))
+            Y = np.zeros((batch_size, size ** 2))
 
-                batch = random.sample(memory, batch_size)
+            batch = random.sample(memory, batch_size)
 
-                for i in range(batch_size):
-                    old_state, action, new_state, reward, terminal = batch[i]
+            for i in range(batch_size):
+                old_state, action, new_state, reward, terminal = batch[i]
 
-                    [old_q_values] = model.predict(np.array([old_state]))
+                [old_q_values] = model.predict(np.array([old_state]))
 
-                    if terminal:
-                        update = reward
-                    else:
-                        [new_q_values] = model.predict(np.array([new_state]))
-                        update = reward + gamma * max(new_q_values)
+                if terminal:
+                    update = reward
+                else:
+                    [new_q_values] = model.predict(np.array([new_state]))
+                    update = reward - gamma * max(new_q_values)
 
-                    old_q_values[action] = update
+                old_q_values[action] = update
 
-                    X[i] = old_state
-                    Y[i] = old_q_values
+                X[i] = old_state
+                Y[i] = old_q_values
 
-                losses[player] = model.train_on_batch(X, Y)
+            losses[1] = model.train_on_batch(X, Y)
 
         ###############################################
         ### Create board and winner_check variables ###
@@ -380,91 +379,98 @@ def learn(size, gamma, batch_size, initial_epsilon, final_epsilon, exploration_e
         #################
         ### Game loop ###
         #################
-        while True:
+        while winner == -1:
             random_move = np.nan
             other_player = 1 - current_player
 
-            #########################
-            ### If game not ended ###
-            #########################
-            if winner == -1:
-                ##################################
-                ### Get move and save q values ###
-                ##################################
-                if current_player in q_players:
-                    i_board = invert_board(board, current_player)
-                    # Compute split board
-                    split_board = get_features(i_board)
+            ##################################
+            ### Get move and save q values ###
+            ##################################
+            i_board = invert_board(board, current_player)
+            # Compute split board
+            split_board = get_features(i_board)
 
-                    # Random move?
-                    random_move = random.random() < epsilon
+            # Random move?
+            random_move = random.random() < epsilon
 
-                    # Get action
-                    if random_move:
-                        action = get_random_action(i_board)  # Limit regret by not choosing completely random action
-                    else:
-                        action = get_action(models[current_player], split_board)
+            # Get action
+            if random_move:
+                action = get_random_action(i_board)  # Limit regret by not choosing completely random action
+            else:
+                action = get_action(model, split_board)
 
-                    # Save action and split board
-                    actions[current_player] = action
-                    states[current_player] = split_board
+            # Get move
+            i_move = get_move_from_action(action, size)
+            move = invert_move(i_move, current_player)
 
-                    # Get move
-                    i_move = get_move_from_action(action, size)
-                    move = invert_move(i_move, current_player)
-                else:
-                    move = get_random_move(board)
-
-                ###################################
-                ### Play move and update winner ###
-                ###################################
-                if can_play_move(board, move):
-                    board[move] = current_player
-                    winner, winner_counter = check_for_winner(move, current_player, winner_matrix, winner_counter)
-                else:
-                    winner = 2
+            ###################################
+            ### Play move and update winner ###
+            ###################################
+            if can_play_move(board, move):
+                board[move] = current_player
+                winner, winner_counter = check_for_winner(move, current_player, winner_matrix, winner_counter)
+            else:
+                winner = 2
 
             ######################
             ### Update players ###
             ######################
-            if (other_player in q_players and winner != 2 and move_count != 0) or \
-                    (current_player in q_players and winner == 2):
-                if current_player in q_players and winner == 2:
-                    #
-                    # Error: Update current player and quit
-                    #
-                    memory = memories[current_player]
-                    action = actions[current_player]
-                    old_state = states[current_player]
-                    reward = rewards["error"]
-                    terminal = True
-                    new_state = None
-                else:
-                    #
-                    # Update other player
-                    #
-                    memory = memories[other_player]
-                    action = actions[other_player]
-                    old_state = states[other_player]
-                    # Get update
-                    if winner == current_player:
-                        reward = rewards["lost"]
-                        terminal = True
-                        new_state = None
-                    elif winner == other_player:
-                        reward = rewards["won"]
-                        terminal = True
-                        new_state = None
-                    else:  # winner == -1
-                        reward = rewards["nothing"]
-                        terminal = False
-                        new_state = get_features(invert_board(board, other_player))
-                #
-                # Save to memory
-                #
-                if len(memory) == memory_size:
-                    memory.popleft()
-                memory.append((old_state, action, new_state, reward, terminal))
+            # if (other_player in q_players and winner != 2 and move_count != 0) or \
+            #         (current_player in q_players and winner == 2):
+            #     if current_player in q_players and winner == 2:
+            #         #
+            #         # Error: Update current player and quit
+            #         #
+            #         memory = memories[current_player]
+            #         action = actions[current_player]
+            #         old_state = states[current_player]
+            #         reward = rewards["error"]
+            #         terminal = True
+            #         new_state = None
+            #     else:
+            #         #
+            #         # Update other player
+            #         #
+            #         memory = memories[other_player]
+            #         action = actions[other_player]
+            #         old_state = states[other_player]
+            #         # Get update
+            #         if winner == current_player:
+            #             reward = rewards["lost"]
+            #             terminal = True
+            #             new_state = None
+            #         elif winner == other_player:
+            #             reward = rewards["won"]
+            #             terminal = True
+            #             new_state = None
+            #         else:  # winner == -1
+            #             reward = rewards["nothing"]
+            #             terminal = False
+            #             new_state = get_features(invert_board(board, other_player))
+
+
+            #
+            # Save to memory
+            #
+            if len(memory) == memory_size:
+                memory.popleft()
+            old_state = split_board
+            new_state = get_features(invert_board(board, other_player))
+
+            if winner == 2:
+                reward = rewards["error"]
+                terminal = True
+            elif winner == current_player:
+                reward = rewards["won"]
+                terminal = True
+            elif winner == other_player:
+                reward = rewards["lost"]
+                terminal = True
+            else:  # winner == -1
+                reward = rewards["nothing"]
+                terminal = False
+
+            memory.append((old_state, action, new_state, reward, terminal))
 
             ###########
             ### Log ###
@@ -483,12 +489,6 @@ def learn(size, gamma, batch_size, initial_epsilon, final_epsilon, exploration_e
             ########################
             index += 1
             move_count += 1
-
-            ######################
-            ### Quit if needed ###
-            ######################
-            if winner == 2 or winner == other_player:
-                break
 
             #####################
             ### Invert player ###
